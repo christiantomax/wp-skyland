24f13cda09e31d2f415360b3f9db5c754d32ae2e
C-DEV <c-dev@developer.com>
2023-10-17 20:55:02 +0700

    fix title

--- wp-content/themes/skyland/header.php
+++ wp-content/themes/skyland/header.php
@@ -29,11 +29,7 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <?php
-        $title = wp_title('|', true, 'right');
-        $updatedTitle = preg_replace('/-/', '', $title).bloginfo('name');
-    ?>
-    <title><?= $updatedTitle?></title>
+    <title><?php wp_title() ?> | <?php bloginfo('name'); ?></title>
 
     <meta name="description" content="<?= $description; ?>">
     <meta name="keywords" content="<?= $keyword; ?>">
